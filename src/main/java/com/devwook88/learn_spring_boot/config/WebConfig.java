c3385a42e1856b5836192fc5c6eb712efa21dfa6
Unknown <lejewk@naver.com>
2018-12-01 20:24:27 +0900

    done

--- src/main/java/com/devwook88/learn_spring_boot/config/WebConfig.java
+++ src/main/java/com/devwook88/learn_spring_boot/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.devwook88.learn_spring_boot.config;
 
+import com.devwook88.learn_spring_boot.interceptor.ExecuteTimeInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -20,5 +23,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Bean
+    public ExecuteTimeInterceptor executeTimeInterceptor() {
+        return new ExecuteTimeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(executeTimeInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/");
     }
 }
